49ba810631fbc366d4f0370af50a460c162578d7
Авцынов Сергей <avtsynov.s@yandex.ru>
2026-06-02 01:05:22 +0300

    fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServicesModule } from './services/services.module';
-import { CategoriesModule } from './categories/categories.module';
 import { CurrenciesModule } from './currencies/currencies.module';
 
 @Module({
-  imports: [ServicesModule, CategoriesModule, CurrenciesModule],
+  imports: [CurrenciesModule],
   controllers: [AppController],
   providers: [AppService],
 })
